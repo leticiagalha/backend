53fdf85f68dd0081dbbf71466eb95c03e021eee7
leticiagalha <leticiasgalha@gmail.com>
2022-08-16 21:48:30 -0300

    terceiro commit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ const express = require("express")
 const cours = require("./Routes/cours")
 const routes = require("./Routes")
 
+require('dotenv/config');
+
 const api = express()
 
 api.use(express.json())
 api.use(cours)
 api.use(routes)
 
-api.listen(8000,() => {
+api.listen(process.env.PORT ||8000,() => {
 
-   console.log("api está rodando na porta 8000") 
+   console.log("api está rodando na porta "+process.env.PORT) 
 })
